164425ddb9b2d84d8ea06f86e390fdf15552e4ac
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-05-13 21:51:50 -0400

    HMR, variable declaration

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const hola = "This is inside a variable, which can be called to be put on the page at any time.";
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My first React app</h1>
+          <h1 className="App-title">Practicing with React.js!</h1>
         </header>
         <p className="App-intro">
           Woohoo!  This is reactive!
+          <h1> {hola} </h1>
         </p>
       </div>
     );
